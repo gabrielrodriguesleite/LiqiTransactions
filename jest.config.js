47e6d3330c2aa47aee33f8cab26507955e5b6019
Gabriel Rodrigues Leite <88800090+gabrielrodriguesleite@users.noreply.github.com>
2025-04-03 15:21:19 -0300

    test - init test config

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/src'],
+  testMatch: [
+    '**/tests/**/*.+(ts|tsx|js',
+    '**/?(*.)+(spec|test).+(ts|tsx|js)'
+  ],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest'
+  },
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
+}
